3c68592f35a04d8d8433744082f7cfded3c31298
axamon <alberto.bregliano@gmail.com>
2017-07-31 22:57:24 +0200

    first

---
+++ leggifilezippati/leggifilezippati.go
@@ -0,0 +1,161 @@
+//Trova file zippati in gzip e li legge
+package leggifilezippati
+
+import (
+	"bufio"
+	"compress/gzip"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+//Contafilein dir prende i nuovi file e li mette in un canale
+func Contafileindir(dir string, fileschan chan string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				//log.Println("event:", event)
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					if strings.Contains(event.Name, "gz") {
+						fileschan <- event.Name
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+	close(fileschan)
+}
+
+func ReadLine(fileschan chan string, strchan chan string) {
+
+	for filezippato := range fileschan {
+		f, err := os.Open(filezippato)
+		if err != nil {
+			fileschan <- filezippato
+			continue
+		}
+		defer f.Close()
+
+		gr, err := gzip.NewReader(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer gr.Close()
+
+		scanner := bufio.NewScanner(gr)
+
+		scanner.Split(bufio.ScanLines)
+
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+			strchan <- scanner.Text()
+		}
+	}
+	close(strchan)
+}
+
+func Leggizip2(fileschan chan string, delimiter rune, filezipchan chan []string) {
+
+	for {
+		select {
+		case filezippato := <-fileschan:
+			f, err := os.Open(filezippato)
+			if err != nil {
+				//log.Fatal(err)
+				fmt.Println("SpeedyGonzales")
+				fileschan <- filezippato
+				continue
+			}
+			defer f.Close()
+
+			gr, err := gzip.NewReader(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer gr.Close()
+
+			cr := csv.NewReader(gr)
+			cr.FieldsPerRecord = -1 //accetta numero di campi variabili
+			cr.Comment = '#'
+			cr.Comma = delimiter //specifica il delimitatore dei campi
+			cr.LazyQuotes = true
+			for {
+				rec, err := cr.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					fmt.Println(err)
+				}
+				filezipchan <- rec
+			}
+			f.Close()
+			close(filezipchan)
+		}
+
+	}
+}
+
+//Leggizip legge un file zippato
+func Leggizip(fileschan chan string, delimiter rune, filezipchan chan []string) {
+
+	for filezippato := range fileschan {
+		f, err := os.Open(filezippato)
+		if err != nil {
+			//fmt.Println(err)
+			//fmt.Println("SpeedyGonzales")
+			fileschan <- filezippato
+			continue
+		}
+		defer f.Close()
+
+		gr, err := gzip.NewReader(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer gr.Close()
+
+		cr := csv.NewReader(gr)
+		cr.FieldsPerRecord = -1 //accetta numero di campi variabili
+		cr.Comment = '#'
+		cr.Comma = delimiter //specifica il delimitatore dei campi
+		cr.LazyQuotes = true
+
+		for {
+			rec, err := cr.Read()
+			if err == io.EOF {
+				break
+
+			}
+			if err != nil {
+				fmt.Println(err)
+			}
+			filezipchan <- rec
+		}
+		f.Close()
+
+	}
+	close(filezipchan)
+
+}
---
+++ logginator.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"logginator/leggifilezippati"
+	"net/url"
+	"os"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	var totbytes int
+	var numfile int
+
+	servizi := make(map[string]int)
+
+	fileschan := make(chan string)     //canale con i file presenti in dir
+	filezipchan := make(chan []string) //canale con le righe del file decompresse
+	//strchan := make(chan string)
+
+	if len(os.Args) != 3 {
+		fmt.Println("Verdica i parametri")
+		os.Exit(1)
+	}
+	dir := os.Args[1] //ad esempio  c:\Projects\Go\src\leggifilezippati
+	status, _ := strconv.Atoi(os.Args[2])
+
+	go leggifilezippati.Contafileindir(dir, fileschan)
+	go leggifilezippati.Leggizip(fileschan, ' ', filezipchan) //passa il channel il nome file e il delimitatore di campo
+	go leggifilezippati.Leggizip(fileschan, ' ', filezipchan)
+	go func() {
+		wg.Done()
+
+		for {
+			time.Sleep(10 * time.Second)
+			keys := []string{}
+			for key := range servizi {
+				keys = append(keys, key)
+			}
+			sort.Strings(keys)
+			for _, val := range keys {
+
+				fmt.Println(val, servizi[val])
+			}
+			fmt.Println()
+		}
+	}()
+	for totale := range filezipchan {
+		u, err := url.Parse(totale[1])
+		if err != nil {
+			//log.Fatal(err)
+			continue
+		}
+		//fmt.Println(totale)
+		httpstatus, err := strconv.Atoi(totale[10])
+		if err != nil {
+			//log.Fatal(err)
+			continue
+		}
+		bytes, err := strconv.Atoi(totale[4])
+		if err != nil {
+			//log.Fatal(err)
+			continue
+		}
+		totbytes = totbytes + bytes
+		if httpstatus >= status {
+			numfile++
+
+			//fmt.Println(httpstatus, u.Host, u.Path, totbytes)
+			servizi[u.Host] = totbytes
+			//data := []byte(totale[0] + totale[1])
+			//fmt.Printf("%x", md5.Sum(data))
+			//fmt.Println(totale)
+
+		}
+		//fmt.Println(numfile)
+	}
+	wg.Wait()
+
+}
